9556ebedd3dfccc3b98ff92bd9d7acf33ad05726
HeliumFork34 <146045415+HeliumFork34@users.noreply.github.com>
2024-06-22 16:29:32 +1200

    deleted:    LICENSE 	modified:   Pentagram 	deleted:    README.md 	modified:   main.cpp 	new file:   res/textures/logo/JPT_Window_Icon.png

--- main.cpp
+++ main.cpp
@@ -8,13 +8,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    PNT::Window window("Ghoul Engine", 1000, 700, 200, 200, ImGuiConfigFlags_ViewportsEnable | ImGuiConfigFlags_DockingEnable);
+    PNT::Window window("JPT Window", 60, 60, 0, 30, ImGuiConfigFlags_ViewportsEnable);
     window.setEventCallback(&eventCallback);
     window.setClearColor(0.25f, 0.25f, 0.25f, 0.25f);
 
+    bool Running = 0;
+
     // Set icon.
-    PNT::image icon("res\\textures\\logo\\ghoul32x32.png");
+    PNT::image icon("res\\textures\\logo\\JPT_Window_Icon.png");
     window.setIcon(icon);
+    icon.loadOnGPU();
 
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
@@ -22,36 +25,49 @@ int main(int argc, char *argv[]) {
         0.0f,  0.5f, 0.0f
     };
 
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    window.hide();
 
-    // Vertex shader.
-    PNT::file vertexFile("res\\shaders\\vertex.glsl");
-    PNT::shader vertexShader(vertexFile.getContents(), GL_VERTEX_SHADER);
-    vertexShader.compile();
-
-    // Fragment shader.
-    PNT::file fragmentFile("res\\shaders\\fragment.glsl");
-    PNT::shader fragmentShader(fragmentFile.getContents(), GL_FRAGMENT_SHADER);
-    fragmentShader.compile();
-
-    // Shader program.
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader.getID());
-    glAttachShader(shaderProgram, fragmentShader.getID());
-    glLinkProgram(shaderProgram);
-
+    bool Button1 = 0, Button2 = 0;
+    unsigned int Button1Size = 100, Button2Size = 100;
     while(!window.shouldClose()) {
         PNT::processEvents();
 
         window.startFrame();
         ImGui::Begin("Controls");
+        ImGui::SetWindowSize(ImVec2(140, 180));
+        if (!Running) {ImGui::SetWindowPos(ImVec2(00,100));}
+        unsigned int ButtonNo = 0;
+        if (Button1) { Button1Size = 105;}
+        else { Button1Size = 100; }
+        if (ImGui::Button("Hello", ImVec2(Button1Size, 30))) {window.minimize();}
+        if (ImGui::IsItemHovered()) { ButtonNo = 1; Button1 = 1; }
+        else { Button1 = 0; }
+ 
+        if (Button2) { Button2Size = 105; }
+        else { Button2Size = 100; }
+        if (ImGui::Button("You Too", ImVec2(Button2Size, 30))) {window.maximize();}
+        if (ImGui::IsItemHovered()) { ButtonNo = 2; Button2 = 1; }
+        else { Button2 = 0; }
+
+        if (ButtonNo == 0) {
+        window.setClearColor(0.25f, 0.25f, 0.25f, 0.25f);
+        }
+        ImGui::Text("ButtonNo=%u", ButtonNo);
+        ImGui::Text("Button1=%u", Button1);
+        ImGui::Text("Button2=%u", Button2);
+
+        ImGui::End();
+
+        ImGui::Begin("Display");
+        ImGui::SetWindowSize(ImVec2(600, 600));
+        if (!Running) {ImGui::SetWindowPos(ImVec2(200,200));}
+
+        icon.ImGuiDraw(ImVec2(200,200));
+
         ImGui::End();
 
         window.endFrame();
+        Running = 1;
     }
 
     PNT::deinit();
