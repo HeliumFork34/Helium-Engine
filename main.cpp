e92a3a0c4e87fae7e76f303900046e9e0eaf7575
HeliumFork34 <146045415+HeliumFork34@users.noreply.github.com>
2024-07-07 17:43:22 +1200

    Added new buttons and extra window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Pentagram.hpp>
+#include <windows.h>
 
 void eventCallback(PNT::Window *window, PNT::windowEvent event) {
 }
@@ -8,7 +9,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    PNT::Window window("JPT Window", 60, 60, 0, 30, ImGuiConfigFlags_ViewportsEnable);
+    PNT::Window window("JPT Window", 600, 60, 0, 30, ImGuiConfigFlags_ViewportsEnable);
     window.setEventCallback(&eventCallback);
     window.setClearColor(0.25f, 0.25f, 0.25f, 0.25f);
 
@@ -18,6 +19,8 @@ int main(int argc, char *argv[]) {
     PNT::image icon("res\\textures\\logo\\JPT_Window_Icon.png");
     window.setIcon(icon);
     icon.loadOnGPU();
+    PNT::image pic1("res\\textures\\logo\\Ghoul.png");
+    pic1.loadOnGPU();
 
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,
@@ -27,14 +30,16 @@ int main(int argc, char *argv[]) {
 
     window.hide();
 
-    bool Button1 = 0, Button2 = 0;
-    unsigned int Button1Size = 100, Button2Size = 100;
+    int BeepFreq = 1000;
+    int32_t I = 0, Cycle = 1000;
+    bool Button1 = 0, Button2 = 0, Button3 = 0, Button3_Active = 0;
+    unsigned int Button1Size = 100, Button2Size = 100, Button3Size = 100;
     while(!window.shouldClose()) {
         PNT::processEvents();
 
         window.startFrame();
         ImGui::Begin("Controls");
-        ImGui::SetWindowSize(ImVec2(140, 180));
+        ImGui::SetWindowSize(ImVec2(140, 260));
         if (!Running) {ImGui::SetWindowPos(ImVec2(00,100));}
         unsigned int ButtonNo = 0;
         if (Button1) { Button1Size = 105;}
@@ -49,25 +54,50 @@ int main(int argc, char *argv[]) {
         if (ImGui::IsItemHovered()) { ButtonNo = 2; Button2 = 1; }
         else { Button2 = 0; }
 
+        if (Button3) { Button3Size = 105; }
+        else { Button3Size = 100; }
+        if (ImGui::Button("Display Me", ImVec2(Button3Size, 30))) {
+            if ( !Button3_Active) { Button3_Active = 1;}
+            else { Button3_Active = 0; } }
+        if (ImGui::IsItemHovered()) { ButtonNo = 3; Button3 = 1; }
+        else { Button3 = 0; }
+
         if (ButtonNo == 0) {
         window.setClearColor(0.25f, 0.25f, 0.25f, 0.25f);
         }
+        ImGui::SliderInt("BeepMe", &BeepFreq, 1000, 7000);
         ImGui::Text("ButtonNo=%u", ButtonNo);
         ImGui::Text("Button1=%u", Button1);
         ImGui::Text("Button2=%u", Button2);
+        ImGui::Text("Button3=%u", Button3);
+        ImGui::Text("I=%i", I);
 
         ImGui::End();
 
-        ImGui::Begin("Display");
-        ImGui::SetWindowSize(ImVec2(600, 600));
-        if (!Running) {ImGui::SetWindowPos(ImVec2(200,200));}
+        if (Button3_Active) {
+            ImGui::Begin("Yeeeehaaaaa");
+            ImGui::SetWindowSize(ImVec2(140, 260));
+            ImGui::SetWindowPos(ImVec2(800,800));
+            ImGui::End();}
 
-        icon.ImGuiDraw(ImVec2(200,200));
+        ImGui::Begin("Cool Stuff");
+        ImGui::SetWindowSize(ImVec2(600, 600));
+        if (!Running) { ImGui::SetWindowPos(ImVec2(200,200)); }
+
+        if ( !Button1 & !Button2 ) { icon.ImGuiDraw(ImVec2(BeepFreq/10,BeepFreq/10)); }
+        else { 
+            if ( I < (Cycle * 0.9) ) { icon.ImGuiDraw(ImVec2(I/2,I/2)); }
+            else { pic1.ImGuiDraw(ImVec2(600,600)); }
+            }
+        if ( Button2 ) { pic1.ImGuiDraw(ImVec2(400,400)); Beep(BeepFreq,50); }
 
         ImGui::End();
 
         window.endFrame();
         Running = 1;
+        if ( Button1 ) { I = I + 1; }
+        else { I = 0; }
+        if (I > Cycle) { I = 1; }
     }
 
     PNT::deinit();
